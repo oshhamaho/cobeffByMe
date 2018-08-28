cb8fe54cccc002e37a756c74af2b65f9036324b8
Nicolas <nicolasabdoul@outlook.fr>
2018-08-28 23:14:14 +0200

    liens accueil

--- v/menu.php
+++ v/menu.php
@@ -1,5 +1,5 @@
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
-    <a class="navbar-brand" href="#">Cobeff</a>
+    <a class="navbar-brand" href="./">Cobeff</a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo02"
             aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
@@ -8,7 +8,7 @@
     <div class="collapse navbar-collapse" id="navbarTogglerDemo02">
         <ul class="navbar-nav mr-auto mt-2 mt-lg-0" id="menu">
             <li class="nav-item active">
-                <a class="nav-link" href="./home.php"><strong>Accueil</strong> <span
+                <a class="nav-link" href="./"><strong>Accueil</strong> <span
                             class="sr-only">(current)</span></a>
             </li>
             <li class="nav-item">
