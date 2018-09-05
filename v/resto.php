ca507c894177a3136466ddcd754b7503370b30c1
Ichtuus <nicolasabdoul@outlook.fr>
2018-09-05 16:29:21 +0200

    ajout de la page restaurant antoinette, reste intégration contenue site actuel. il reste les pages contact et vue des 7 formations a adapter

--- v/resto.php
+++ v/resto.php
@@ -10,6 +10,142 @@
 <div class="row">
     <h1>Le restaurant Antoinette</h1>
 
+    <div class="card-columns">
+        <div class="card">
+            <img class="card-img-top" src="./assets/img/Photosachetees/restaurant/resto.jpg" alt="Card image cap">
+            <div class="card-body">
+                <h5 class="card-title">C'est quoi le restaurant Antoinette ?</h5>
+                <p class="card-text"><p>Antoinette, le restaurant d’application de la Cobeff, est un lieu d’apprentissage et de formation. En être client, c’est participer à notre action d’insertion.</p>
+
+
+
+                    <p>Pour les réservations, uniquement par téléphone au : <br><center>02/250.35.60</center></p>
+            </div>
+        </div>
+        <div class="card p-3">
+            <blockquote class="blockquote mb-0 card-body">
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere erat a ante.</p>
+                <footer class="blockquote-footer">
+                    <small class="text-muted">
+                        Someone famous in <cite title="Source Title">Source Title</cite>
+                    </small>
+                </footer>
+            </blockquote>
+        </div>
+        <div class="card">
+            <img class="card-img-top" src="./assets/img/Photosachetees/restaurant/resto2.jpg" alt="Card image cap">
+            <div class="card-body">
+                <h5 class="card-title">Le menu</h5>
+                <p class="card-text">Retrouver ici le menu hebdomadaire du restaurant Antoinette !</p>
+            </div>
+        </div>
+        <div class="card bg-primary text-white text-center p-3">
+            <blockquote class="blockquote mb-0">
+                <p>Cher(es) client(es),</p>
+
+                    ​
+
+                    <p>Nous voilà repartis pour une année de formation cuisine.<br>
+
+                    ​
+
+                    A cet effet, n'hésitez pas à venir nous rejoindre pour venir déguster les plats préparés par nos stagiaires sous les ordres avisés de nos deux chefs.<br>
+
+                    ​
+
+                <p>Nouveauté de cette année:</p>
+
+                    ​
+
+                   <p> N'hésitez pas à nous suivre sur notre compte Facebook en cliquant sur le logo ci dessous
+
+                    ​</p>
+                <footer class="footer">
+                    <small>
+                        <a href="https://www.facebook.com/Restaurant-Antoinette-Cobeff-629607177237717/" id="fb" type="button" class="btn-floating btn-lg btn-fb"><i class="fab fa-facebook-f"></i></a>
+                    </small>
+                </footer>
+            </blockquote>
+        </div>
+        <div class="card text-center">
+            <div class="card-body">
+                <h5 class="card-title">Les tarifs</h5>
+                <div class="card bg-primary text-white text-center p-3">
+                    <blockquote class="blockquote mb-0">
+                        <small>
+
+                            <p>Menu du jour</p>
+                            <em>(Potage ou Entrée + Plat +Dessert)</em> 10,00€
+
+                       <p>​Potage ou Entrée</p>2,50€
+
+                            <p>​Grande Entrée</p>5,00€
+
+                            <p>​Plat</p> 6,50€
+
+                            <p>​Suggestion</p> 6,50€
+
+                            <p>​Supplément légumes</p> 1,00€
+
+                            <p>​Dessert</p> 2,00€<
+
+                            <p>​Soft (33cl.)</p> 1,20€
+
+                            <p>​Eau plate (50cl.)</p> 1,20€
+
+                            <p>​Vin - le verre</p> 1,80
+
+                           <p>​Vin - 1/4</p> 3,50€
+
+                            <p>​Vin - 1/2</p> 7,50€
+
+                           <p>​Vin - la bouteille</p> 10,00€
+
+                            <p>​Café/Thé</p> 1,20€
+
+                        </small>
+                        <footer class="footer">
+                            <small>
+                            </small>
+                        </footer>
+                    </blockquote>
+                </div>
+            </div>
+        </div>
+        <div class="card">
+            <img class="card-img" src="./assets/img/Photosachetees/restaurant/resto3.jpg" alt="Card image">
+        </div>
+        <div class="card p-3 text-right">
+            <blockquote class="blockquote mb-0">
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere erat a ante.</p>
+                <footer class="blockquote-footer">
+                    <small class="text-muted">
+                        Someone famous in <cite title="Source Title">Source Title</cite>
+                    </small>
+                </footer>
+            </blockquote>
+        </div>
+        <div class="card">
+            <div class="card-body">
+                <h5 class="card-title">Horaire et accés</h5>
+
+                <p> Service assuré de 12h à 13h45.</p>
+                <p class="card-text">
+                    <strong>Adresse:<br></strong>
+
+                    Rue Philomène 37
+
+                    1030 Bruxelles.<br>
+
+
+
+                    Bus 65 et 66, tram 25 arrêt "Coteaux".
+
+                    Bus 59 arrêt "Geefs".</p>
+            </div>
+        </div>
+    </div>
+
 </div>
 
 </div>
